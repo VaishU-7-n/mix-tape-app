b9c077715ae060862a1efd6a93ea10681f3b56c0
vaishnavi <nelantivaishnavi@gmail.com>
2026-06-29 22:40:50 +0530

    "final css changes to home page"

--- mixtape-app/src/pages/Home/Home.jsx
+++ mixtape-app/src/pages/Home/Home.jsx
@@ -1,32 +1,35 @@
 import { Link } from "react-router-dom";
 import './Home.css'
-export function Home()
-{
-    return(
-        <>
-            <header>
-                <title>
-                    Home - page
-                </title>
-            </header>
-           <div className="container">
-             <div className="rectangle-1"></div>
-                <div className="rectangle-2"> <p className="text-starter">Create your own mix-tape</p><div ><Link to ="/create"><button className = "get-started-button">Get Started →</button></Link></div></div>
+
+export function Home() {
+    return (
+        <div className="page-wrapper">
+           <div className="casette-border">
+             <div className="container">
+                
+
+                <div className="rectangle-6"></div>
+                <div className="rectangle-5"><div className="triangle1"> </div> <div className="triangle2"> </div></div>
+
+                <p className="text-starter">Create your own mix-tape</p>
+
+                <Link to="/create">
+                    <button className="get-started-button">Get Started →</button>
+                </Link>
+
                 <div className="casette-center"></div>
-                <div className = "casette-circle-1"></div>
-                <div className = "casette-circle-2"></div>
+                <div className="casette-circle-1"></div>
+                <div className="casette-circle-2"></div>
                 <div className="casette-center-rect"></div>
+
                 <div className="bottom-rectangle">
-                    <div className="circle-1"></div>
-                     <div className="circle-2"></div>
-                      <div className="circle-3"></div>
-                       <div className="circle-4"></div>
+                    <div className="ellipse-3"></div>
+                    <div className="ellipse-5"></div>
+                    <div className="ellipse-6"></div>
+                    <div className="ellipse-4"></div>
                 </div>
-                
+            </div>
            </div>
-               
-                
-           
-        </>
+        </div>
     );
 }
